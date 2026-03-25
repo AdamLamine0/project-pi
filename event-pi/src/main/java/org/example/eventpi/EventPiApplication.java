504e76261bbe7b3f7d489e166fc9c2b66d4d32c6
zouari farah <131656550+farahz12@users.noreply.github.com>
2026-03-25 22:39:58 +0100

    event0

---
+++ event-pi/src/main/java/org/example/eventpi/EventPiApplication.java
@@ -0,0 +1,13 @@
+package org.example.eventpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventPiApplication.class, args);
+    }
+
+}
